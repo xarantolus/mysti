afeb43503cab87ad3b04b8667e421ee8349ba135
Philipp Erhardt <philipp.erhardt@tum.de>
2023-12-26 21:57:49 +0100

    Add a menu

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,6 @@
+use common::name::client_name;
+use dialoguer::FuzzySelect;
+
 mod rest;
 
 fn main() {
@@ -6,5 +9,31 @@ fn main() {
     let clients =
         rest::fetch_connected_clients(&config).expect("Failed to fetch connected clients");
 
-    println!("{:#?}", clients);
+    if clients.is_empty() {
+        println!("No clients are currently connected");
+        return;
+    }
+
+    let current_client_name = client_name();
+
+    // get index of the first client that is not ourselfes
+    let default_idx = clients
+        .iter()
+        .position(|client| client.name != current_client_name)
+        .unwrap_or(0);
+
+    let selection = if clients.len() <= 1 {
+        0
+    } else {
+        FuzzySelect::new()
+            .with_prompt("What do you choose?")
+            .items(&clients)
+            .default(default_idx)
+            .interact()
+            .unwrap()
+    };
+
+    println!("Client: {}", clients[selection]);
+
+    // TODO: Now actually do something with it
 }
--- common/src/types.rs
+++ common/src/types.rs
@@ -1,6 +1,23 @@
+use std::{fmt::{Display, Formatter}, time::SystemTime};
+
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct ConnectedClientInfo {
     pub name: String,
     pub id: usize,
     pub connected_at: std::time::SystemTime,
+}
+
+impl Display for ConnectedClientInfo {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{} (id: {}, connected {}s ago)",
+            self.name,
+            self.id,
+            SystemTime::now()
+                .duration_since(self.connected_at)
+                .unwrap()
+                .as_secs()
+        )
+    }
 }
